3be121726aeb49945bc3edc6cc3c08c1e543ec3b
PieMenard <pinedo.m.gustavo@gmail.com>
2025-03-17 19:57:13 -0400

    setup project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { useQuery } from '@tanstack/react-query';
+import './App.css';
+
+function App() {
+  console.log(useQuery);
+  return (
+    <>
+      <h1 className="text-2xl">Hello</h1>
+    </>
+  );
+}
+
+export default App;
