c55749a44c9cdd28745d694a940129420676897c
PieMenard <pinedo.m.gustavo@gmail.com>
2025-03-17 23:30:52 -0400

    setup react-query

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,40 @@
 import { useQuery } from '@tanstack/react-query';
 import './App.css';
 
+type Post = {
+  id: number;
+  userId: number;
+  title: string;
+  body: string;
+};
+
+const fetchPosts = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+  if (!response.ok) {
+    throw new Error('Error fetching data');
+  }
+  return response.json();
+};
+
 function App() {
-  console.log(useQuery);
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ['posts'],
+    queryFn: fetchPosts,
+  });
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
+  if (isError) {
+    return <p>Something went wrong.</p>;
+  }
+
   return (
     <>
-      <h1 className="text-2xl">Hello</h1>
+      {data.map((post: Post) => (
+        <p key={post.id}>{post.title}</p>
+      ))}
     </>
   );
 }
